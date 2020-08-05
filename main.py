51585c6af96f66794098ef465fd03e93a7acad66
Антон Поляков <example@email.com>
2020-08-05 19:26:44 +0300

    added new function say_goodbye

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
 
 
+def say_goodbye(name):
+    print(f'Bye, {name}')
 # Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
+    say_goodbye('Max')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
